2a4b991e2cbd1fc88f2df0ef823d6a5de6a9c474
FoxConNM <victormenezes1121@gmail.com>
2023-03-03 18:46:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Contador = styled.section`
+  display: flex;
+  justify-content: center;
+
+  align-items: center;
+  flex-direction: column;
+  width: 50vh;
+  height: 80vh;
+  background-color: tomato;
+  border-radius: 5%;
+  h1{
+     font-size: 90px;
+     color: #ffff;
+  }
+`
+const Botoes = styled.div`
+display: flex;
+gap: 60px;
+`
+const Btn = styled.button`
+border-radius: 100%;
+width: 80px;
+height: 80px;
+font-size: 30px;
+border-style: none;
+color: tomato;
+`
+
+class App extends Component {
+  state={
+    contador:0
 }
 
+Soma = () =>{
+    this.setState({
+        contador: this.state.contador < 10 ? this.state.contador + 1:this.state.contador
+    })
+}
+Diminuir = () =>{
+    this.setState({
+        contador: this.state.contador > 0 ? this.state.contador - 1:0
+    })
+}
+
+render(){
+    return(
+        <>
+        <Contador>
+            <h1>{this.state.contador}</h1>
+            <Botoes>
+            <Btn onClick={this.Soma}>+</Btn>
+            <Btn onClick={this.Diminuir}>-</Btn>
+            </Botoes>
+        </Contador>
+
+        </>
+    )
+}
+}
 export default App;
